15266d45ee6e7fd37e6e2b658319a4471b0378ee
Logiqx <software@mikeg.me.uk>
2021-11-16 22:34:40 +0000

    Mount events folder in Docker

--- bin/env.sh
+++ bin/env.sh
@@ -5,10 +5,7 @@ WORK_DIR=/home/jovyan/work/$PROJ_NAME
 run_py_script()
 {
   docker run -it --rm \
-         --mount type=bind,src=$PROJ_DIR/2018,dst=$WORK_DIR/2018,readonly \
-         --mount type=bind,src=$PROJ_DIR/2019,dst=$WORK_DIR/2019,readonly \
-         --mount type=bind,src=$PROJ_DIR/2021,dst=$WORK_DIR/2021,readonly \
-         --mount type=bind,src=$PROJ_DIR/2022,dst=$WORK_DIR/2022,readonly \
+         --mount type=bind,src=$PROJ_DIR/events,dst=$WORK_DIR/events,readonly \
          --mount type=bind,src=$PROJ_DIR/config,dst=$WORK_DIR/config,readonly \
          --mount type=bind,src=$PROJ_DIR/docs,dst=$WORK_DIR/docs \
          ${PROJ_NAME}:${IMAGE_TAG:-latest} python/$1
