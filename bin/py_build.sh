a4018031722721c2cf152d84be52be75d63110f6
Logiqx <software@mikeg.me.uk>
2022-01-04 10:37:11 +0000

    Improve country validation / fixes

--- bin/py_build.sh
+++ bin/py_build.sh
@@ -11,6 +11,7 @@ DOCKER_BUILDKIT=1 docker build . --build-arg LOGIQX_DEBUG -t $IMAGE_NAME:$IMAGE_
 # Copy the library scripts
 docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/constants.py >$PROJ_DIR/python/constants.py
 docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/common.py >$PROJ_DIR/python/common.py
+docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/country.py >$PROJ_DIR/python/country.py
 docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/name.py >$PROJ_DIR/python/name.py
 docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/fuzzy.py >$PROJ_DIR/python/fuzzy.py
 docker run --rm --entrypoint cat $IMAGE_NAME:$IMAGE_TAG $WORK_DIR/python/entrant.py >$PROJ_DIR/python/entrant.py
