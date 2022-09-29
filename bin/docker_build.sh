348f2902451cc17015a4524bdd1527a3411b2df7
Logiqx <software@mikeg.me.uk>
2022-09-29 17:58:32 +0100

    Disable series test during Docker build

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -8,11 +8,24 @@ IMAGE_TAG=$(git rev-parse --short=12 HEAD)
 # Docker Build
 DOCKER_BUILDKIT=1 docker build . --build-arg LOGIQX_DEBUG -t $IMAGE_NAME:$IMAGE_TAG
 
+# Run unit tests
+run_py_script country.py
+run_py_script name.py
+run_py_script fuzzy.py
+run_py_script entrant.py
+run_py_script filter.py
+run_py_script reports.py
+run_py_script speedrun.py
+run_py_script period.py
+run_py_script course.py
+run_py_script session.py
+run_py_script event.py
+
 # Run daily reporting
 run_py_script results.py
 
 # Run series reporting
-run_py_script series.py
+#run_py_script series.py
 
 # Apply release tag, removing earlier tag / image if required
 if [ -n "$(docker image ls -q $IMAGE_NAME:$FINAL_TAG)" ]
