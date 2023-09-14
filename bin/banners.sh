520cfdef151dcbad36d7ac042590dbbba1f66064
WSW <software@mikeg.me.uk>
2023-09-14 10:18:44 +0100

    Add basic images for 2023

--- bin/banners.sh
+++ bin/banners.sh
@@ -1,6 +1,6 @@
-for year in 1998 1999 2000 2001 2002 2003 2004 2005 2006 2009 2014 2015 2016 2017 2018 2019 2020 2021 2022
+for year in 2023
 do
     convert docs/img/wswlogo_print.png  -gravity North -pointsize 14 -fill 'rgb(64,65,44)' -antialias -font "docs/fonts/Arkitech Medium.ttf" -annotate -49+27 "$year" docs/img/wswlogo_$year.png
 done
 
-convert docs/img/wswlogo_print.png  -gravity North -pointsize 12 -fill 'rgb(64,65,44)' -antialias -font "docs/fonts/Arkitech Bold.ttf" -annotate -49+18 "HISTORY" -pointsize 8 -annotate -49+30 "1998 - 2021" docs/img/wswlogo_all.png
+convert docs/img/wswlogo_print.png  -gravity North -pointsize 12 -fill 'rgb(64,65,44)' -antialias -font "docs/fonts/Arkitech Bold.ttf" -annotate -49+18 "HISTORY" -pointsize 8 -annotate -49+30 "1998 - 2022" docs/img/wswlogo_all.png
