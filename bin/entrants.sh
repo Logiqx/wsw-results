9214b60cd0523d2436d9c5cdcd428e64d357a4c0
Logiqx <software@mikeg.me.uk>
2023-09-27 08:39:57 +0100

    Remove extra whitespace from entries CSV

--- bin/entrants.sh
+++ bin/entrants.sh
@@ -1,5 +1,9 @@
 # Project Env
 . $(dirname $0)/env.sh
 
+# Remove extra whitespace
+YEAR=$(date +%Y)
+sed -i 's/ *$//;s/ *,/,/g' events/$YEAR/config/entries.csv
+
 # Run the daily results
 run_py_script entrants.py
