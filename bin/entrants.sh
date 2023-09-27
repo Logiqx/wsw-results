9a4f5de2facf02e35d8b6330ce5596a1759a373d
Logiqx <software@mikeg.me.uk>
2023-09-27 08:42:47 +0100

    Remove extra whitespace within sponsors from entries CSV

--- bin/entrants.sh
+++ bin/entrants.sh
@@ -3,7 +3,7 @@
 
 # Remove extra whitespace
 YEAR=$(date +%Y)
-sed -i 's/ *$//;s/ *,/,/g' events/$YEAR/config/entries.csv
+sed -i 's/ *$//;s/ *"/"/g;s/ *,/,/g' events/$YEAR/config/entries.csv
 
 # Run the daily results
 run_py_script entrants.py
