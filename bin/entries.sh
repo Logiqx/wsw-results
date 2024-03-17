e7cd3c256e084b3b45fca9f3e93c6662643de974
Logiqx <software@mikeg.me.uk>
2024-03-17 06:17:16 +0000

    Remove whitespace from entries for all events

--- bin/entries.sh
+++ bin/entries.sh
@@ -1,9 +1,11 @@
 # Project Env
 . $(dirname $0)/env.sh
 
-# Remove extra whitespace
-YEAR=$(date +%Y)
-sed -i 's/ *$//;s/ *"/"/g;s/ *,/,/g' events/$YEAR/config/entries.csv
+# Remove extra whitespace from latest entries
+for ENTRIES in $(ls events/*/config/entries.csv)
+do
+  sed -i 's/ *$//;s/ *"/"/g;s/ *,/,/g' $ENTRIES
+done
 
 # Run the daily results
 run_py_script entries.py
