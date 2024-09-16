89d5de0a829d5d1fb2ec73a5495a0b40c3652f88
Logiqx <software@mikeg.me.uk>
2024-09-16 20:08:34 +0100

    Add banner for 2024

--- bin/banners.sh
+++ bin/banners.sh
@@ -1,6 +1,6 @@
-for year in 2023
+for year in 2024
 do
-    convert docs/img/wswlogo_print.png  -gravity North -pointsize 14 -fill 'rgb(64,65,44)' -antialias -font "docs/fonts/Arkitech Medium.ttf" -annotate -49+27 "$year" docs/img/wswlogo_$year.png
+    convert docs/img/wswlogo_print.png -gravity NorthWest -pointsize 14 -fill 'rgb(64,65,44)' -antialias -font "docs/fonts/Arkitech Medium.ttf" -gravity NorthWest -annotate +118+27 "$year" docs/img/wswlogo_$year.png
 done
 
-convert docs/img/wswlogo_print.png  -gravity North -pointsize 12 -fill 'rgb(64,65,44)' -antialias -font "docs/fonts/Arkitech Bold.ttf" -annotate -49+18 "HISTORY" -pointsize 8 -annotate -49+30 "1998 - 2022" docs/img/wswlogo_all.png
+convert docs/img/wswlogo_print.png -gravity NorthWest -pointsize 12 -fill 'rgb(64,65,44)' -antialias -font "docs/fonts/Arkitech Bold.ttf" -annotate +112+18 "HISTORY" -pointsize 8 -annotate +112+30 "1998 - 2023" docs/img/wswlogo_all.png
