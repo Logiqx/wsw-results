31ee93f45a05c575709509950f650faa892d021d
Logiqx <software@mikeg.me.uk>
2024-09-17 08:45:56 +0100

    Re-order lines in shell script

--- bin/fix_2024.sh
+++ bin/fix_2024.sh
@@ -12,22 +12,22 @@ fix_data "E gland" "United Kingdom"
 fix_data "Great Britain" "United Kingdom"
 fix_data "U.K." "United Kingdom"
 fix_data "Slow,," "Slow,United Kingdom,"
-fix_data "Standish,," "Standish,United Kingdom,"
 fix_data "Speedfine,," "Speedfine,United Kingdom,"
+fix_data "Standish,," "Standish,United Kingdom,"
 fix_data "USA" "United States"
 
 # Fix names such as George Fulton Fulton
 fix_data "Chris Cleland" "Cleland"
-fix_data "Jason Morton,Jason Morton" "Jason,Morton"
 fix_data "George Fulton" "Fulton"
+fix_data "Jason Morton,Jason Morton" "Jason,Morton"
 fix_data "Neil Binding" "Binding"
 
 # Use previous names
-fix_data "Robert,Dunn" "Rob,Dunn"
-fix_data "Joseph,Trunkfield" "Joe,Trunkfield"
-fix_data "JP,Standish" "John-Paul,Standish"
 fix_data "David,Williams" "Dave,Williams"
 fix_data "matt,york" "Matthew,York"
+fix_data "Joseph,Trunkfield" "Joe,Trunkfield"
+fix_data "JP,Standish" "John-Paul,Standish"
+fix_data "Robert,Dunn" "Rob,Dunn"
 fix_data "Robert,Spagnoletti" "Bob,Spagnoletti"
 fix_data "Sav,Sav" "Sav,Salvage"
 
