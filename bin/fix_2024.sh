e8c39e4043e66576070732cbeb9d092eed5e5e18
Logiqx <software@mikeg.me.uk>
2024-09-17 16:19:52 +0100

    Patch names in motions.csv

--- bin/fix_2024.sh
+++ bin/fix_2024.sh
@@ -1,35 +1,44 @@
-fix_data()
+fix_entrants()
 {
-    OLD=$1
-    NEW=$2
-    sed -i "s/$OLD/$NEW/" events/2024/config/entrants.csv
+    sed -i "s/$1/$2/" events/2024/config/entrants.csv
+}
+
+fix_motions()
+{
+    sed -i "s/$1/$2/" events/2024/config/motions.csv
+}
+
+fix_all()
+{
+    sed -i "s/$1/$2/" events/2024/config/entrants.csv
+    sed -i "s/$1/$2/" events/2024/config/motions.csv
 }
 
 # Fix countries
-fix_data Belgique Belgium
-fix_data Nederland Netherlands
-fix_data "E gland" "United Kingdom"
-fix_data "Great Britain" "United Kingdom"
-fix_data "U.K." "United Kingdom"
-fix_data "Slow,," "Slow,United Kingdom,"
-fix_data "Speedfine,," "Speedfine,United Kingdom,"
-fix_data "Standish,," "Standish,United Kingdom,"
-fix_data "USA" "United States"
+fix_entrants Belgique Belgium
+fix_entrants Nederland Netherlands
+fix_entrants "E gland" "United Kingdom"
+fix_entrants "Great Britain" "United Kingdom"
+fix_entrants "U.K." "United Kingdom"
+fix_entrants "Slow,," "Slow,United Kingdom,"
+fix_entrants "Speedfine,," "Speedfine,United Kingdom,"
+fix_entrants "Standish,," "Standish,United Kingdom,"
+fix_entrants "USA" "United States"
 
 # Fix names such as George Fulton Fulton
-fix_data "Chris Cleland" "Cleland"
-fix_data "George Fulton" "Fulton"
-fix_data "Jason Morton,Jason Morton" "Jason,Morton"
-fix_data "Neil Binding" "Binding"
+fix_all "Chris Cleland" "Cleland"
+fix_all "George Fulton" "Fulton"
+fix_all "Jason Morton,Jason Morton" "Jason,Morton"
+fix_all "Neil Binding" "Binding"
 
 # Use previous names
-fix_data "David,Williams" "Dave,Williams"
-fix_data "matt,york" "Matthew,York"
-fix_data "Joseph,Trunkfield" "Joe,Trunkfield"
-fix_data "JP,Standish" "John-Paul,Standish"
-fix_data "Robert,Dunn" "Rob,Dunn"
-fix_data "Robert,Spagnoletti" "Bob,Spagnoletti"
-fix_data "Sav,Sav" "Sav,Salvage"
+fix_all "David,Williams" "Dave,Williams"
+fix_all "matt,york" "Matthew,York"
+fix_all "Joseph,Trunkfield" "Joe,Trunkfield"
+fix_all "JP,Standish" "John-Paul,Standish"
+fix_all "Robert,Dunn" "Rob,Dunn"
+fix_all "Robert,Spagnoletti" "Bob,Spagnoletti"
+fix_all "Sav,Sav" "Sav,Salvage"
 
 # Check for any suspect names!
 grep 'M[rs][a-z]*,[A-Za-z]*,[A-Za-z]* [A-Za-z]*' events/2024/config/entrants.csv
