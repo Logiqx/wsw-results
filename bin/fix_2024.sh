d76aa08fe18c18841642aeb6f09e008e6b54b4a1
Logiqx <software@mikeg.me.uk>
2024-10-01 09:14:24 +0100

    Fix most name issues in Joomla

--- bin/fix_2024.sh
+++ bin/fix_2024.sh
@@ -6,34 +6,15 @@ fix_all()
 }
 
 # Fix countries
-fix_all Belgique Belgium
-fix_all Nederland Netherlands
-fix_all ",DE," ",Germany,"
-fix_all "dosret" "United Kingdom"
-fix_all "E gland" "United Kingdom"
-fix_all "Great Britain" "United Kingdom"
-fix_all "U.K." "United Kingdom"
-fix_all "Slow,," "Slow,United Kingdom,"
-fix_all "Speedfine,," "Speedfine,United Kingdom,"
-fix_all "Standish,," "Standish,United Kingdom,"
-fix_all "USA" "United States"
+fix_all ",UK," ",United Kingdom,"
+fix_all ",U.K.," ",United Kingdom,"
+fix_all ",USA," ",United States,"
 
 # Fix names such as George Fulton Fulton
-fix_all "Cash James Cox" "Cash James"
 fix_all "Chris Cleland" "Cleland"
-fix_all "George Fulton" "Fulton"
 fix_all "Jason Morton,Jason Morton" "Jason,Morton"
-fix_all "Neil Binding" "Binding"
-fix_all "Theo Cash Cox" "Theo Cash"
 
 # Use previous names
-fix_all "David,Williams" "Dave,Williams"
-fix_all "matt,york" "Matthew,York"
-fix_all "Joseph,Trunkfield" "Joe,Trunkfield"
-fix_all "JP,Standish" "John-Paul,Standish"
-fix_all "Robert,Dunn" "Rob,Dunn"
-fix_all "Robert,Spagnoletti" "Bob,Spagnoletti"
-fix_all "Sav,Sav" "Sav,Salvage"
 
 # Check for any suspect names!
 grep 'M[rs][a-z]*,[A-Za-z]*,[A-Za-z]* [A-Za-z]*' events/2024/config/entrants.csv
