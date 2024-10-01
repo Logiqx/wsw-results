0eded59091a21d8b034fc8c8a06e0fe30eb5143f
Logiqx <software@mikeg.me.uk>
2024-10-01 09:29:18 +0100

    Tweak fix script

--- bin/fix_2024.sh
+++ bin/fix_2024.sh
@@ -1,14 +1,12 @@
 fix_all()
 {
     sed -i "s/$1/$2/" events/2024/config/entries.csv
-    #sed -i "s/$1/$2/" events/2024/config/entrants.csv
-    #sed -i "s/$1/$2/" events/2024/config/motions.csv
 }
 
 # Fix countries
-fix_all ",UK," ",United Kingdom,"
-fix_all ",U.K.," ",United Kingdom,"
-fix_all ",USA," ",United States,"
+fix_all ",UK *," ",United Kingdom,"
+fix_all ",U.K. *," ",United Kingdom,"
+fix_all ",USA *," ",United States,"
 
 # Fix names such as George Fulton Fulton
 fix_all "Chris Cleland" "Cleland"
@@ -16,5 +14,5 @@ fix_all "Jason Morton,Jason Morton" "Jason,Morton"
 
 # Use previous names
 
-# Check for any suspect names!
+# Check for any suspect family names!
 grep 'M[rs][a-z]*,[A-Za-z]*,[A-Za-z]* [A-Za-z]*' events/2024/config/entrants.csv
