1290dea5b6c46078fae2f0d461cedc8ba4e89931
Logiqx <software@mikeg.me.uk>
2025-10-18 19:21:56 +0100

    Improve distance script

--- bin/distance.sh
+++ bin/distance.sh
@@ -1,1 +1,7 @@
-grep "downloaded" events/2025/logs/server.log | sed 's/_.*oao / /' | awk '{a[$3] += $10} END {for (i in a) print i, a[i]}' | sort -k2 -n -r | head -10
+for i in events/*/logs/server.log
+do
+    echo $i
+    grep "downloaded" $i | awk '{a += $10} END {print "TOT DIST  " a " km"}'
+    grep "downloaded" $i | sed 's/_.*oao / /' | awk '{a[$3] += $10} END {for (i in a) print i, a[i]}' | sort -k2 -n -r | head -10
+    echo
+done
