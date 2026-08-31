b6f852c7ae0dc97f119e82dec3c71e786840e6b6
Logiqx <software@mikeg.me.uk>
2026-08-31 16:05:43 +0100

    Fix Mark Salvage

--- bin/fix_2026.sh
+++ bin/fix_2026.sh
@@ -29,6 +29,7 @@ fix_all ",Jean-marc," ",Jean-Marc,"
 fix_all ",Jon,Wills," ",Jonathan,Wills,"
 fix_all ",Robert,Longstaff," ",Rob,Longstaff,"
 fix_all ",Robert,Spagnoletti," ",Bob,Spagnoletti,"
+fix_all ",Sav,Salvage," ",Mark,Salvage,"
 fix_all ",Stephen,Thompson," ",Steve,Thompson,"
 
 # Fix countries for individuals
