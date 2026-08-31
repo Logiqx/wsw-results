f43af107b65ab0312ec572f55febba4a9fe94f4b
Logiqx <software@mikeg.me.uk>
2026-08-31 16:01:10 +0100

    First batch of entrants for 2026

--- bin/banners.sh
+++ bin/banners.sh
@@ -1,4 +1,4 @@
-for year in 2025
+for year in 2026
 do
     convert docs/img/wswlogo_print.png -gravity NorthWest -pointsize 14 -fill 'rgb(64,65,44)' -antialias -font "docs/fonts/Arkitech Medium.ttf" -gravity NorthWest -annotate +118+27 "$year" docs/img/wswlogo_$year.png
 done
---
+++ bin/fix_2026.sh
@@ -0,0 +1,41 @@
+fix_all()
+{
+    sed -i "s/$1/$2/i" events/2026/config/entries.csv
+}
+
+# Remove extra whitespace from latest entries
+for ENTRIES in $(ls events/*/config/entries.csv)
+do
+  sed -i 's/ *$//;s/ *"/"/g;s/ *,/,/g' $ENTRIES
+done
+
+# Fix countries
+fix_all ",BEL *," ",Belgium,"
+fix_all ",FRA *," ",France,"
+fix_all ",GER *," ",Germany,"
+fix_all ",GBR *," ",United Kingdom,"
+fix_all ",NED *," ",Netherlands,"
+fix_all ",POL *," ",Poland,"
+fix_all ",POR *," ",Portugal,"
+fix_all ",USA *," ",United States,"
+
+# Fix name typos
+fix_all ",Will Trossell," ",Trossell,"
+
+# Fix names for consistency with past events
+fix_all ",David,Williams," ",Dave,Williams,"
+fix_all ",JP,Standish," ",John-Paul,Standish,"
+fix_all ",Jean-marc," ",Jean-Marc,"
+fix_all ",Jon,Wills," ",Jonathan,Wills,"
+fix_all ",Robert,Longstaff," ",Rob,Longstaff,"
+fix_all ",Robert,Spagnoletti," ",Bob,Spagnoletti,"
+fix_all ",Stephen,Thompson," ",Steve,Thompson,"
+
+# Fix countries for individuals
+fix_all ",Scott,Harrison,United Kingdom," ",Scott,Harrison,Ireland,"
+
+# Fix groups
+fix_all "Weymouth and Portland Kitesurfing Club" ""
+
+# Check for any suspect family names!
+grep 'M[rs][a-z]*,[A-Za-z]*,[A-Za-z]* [A-Za-z]*' events/2026/config/entrants.csv
