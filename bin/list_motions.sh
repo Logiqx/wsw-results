dfc3c3bd04db534091ecb012a011514099f067c7
Logiqx <software@mikeg.me.uk>
2026-09-26 12:06:57 +0100

    More work on Motion list

--- bin/list_motions.sh
+++ bin/list_motions.sh
@@ -1,3 +1,3 @@
 YEAR=$(date +%Y)
 
-csvcut -c ID,Title,"First Name","Family Name","Motion for Boat","Motion for Kiteboard","Motion for Sailboard (fin)","Motion for Sailboard (foil)","Motion for Wingboard" events/2026/config/entries.csv
+csvcut -c ID,Title,"First Name","Family Name","Youth Weekend","UKWA Member","Entrants Own Motion Mini","Motion for Boat","Motion for Kiteboard","Motion for Sailboard (fin)","Motion for Sailboard (foil)","Motion for Wingboard" events/2026/config/entries.csv
